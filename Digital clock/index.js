fafad7a6c4a2378102fc4d7702ef8825776b5e8a
Sourav <souravlakshan6@gmail.com>
2023-01-04 11:41:06 +0530

    ampm updated

--- Digital clock/index.js
+++ Digital clock/index.js
@@ -7,11 +7,11 @@ function updateClock(){
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s= new Date().getSeconds()
-    let ampm = "PM"
+    let ampm = "AM"
 
     if(h > 12) {
         h = h - 12
-        ampm ="AM"
+        ampm ="PM"
     }
 
     h = h < 10 ? "0" + h : h;
